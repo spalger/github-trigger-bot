61dc5941103cd2cc3d80b95cf5b8beeb37d176a0
spalger <email@spalger.com>
2016-07-06 23:16:51 -0600

    library boilerplate

---
+++ src/__tests__/index.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+
+import { expect } from 'chai'
+
+describe('tests', () => {
+  it('works', () => {
+    expect(true).to.equal(true)
+  })
+})
---
+++ src/test-support/index.js
@@ -0,0 +1,2 @@
+require('source-map-support')
+require('babel-register')
