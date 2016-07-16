790583a39a96f0f34627fa9266e4f8c8174c7a39
spalger <email@spalger.com>
2016-07-16 16:42:49 -0700

    transform snake_case keys to camelCase

--- src/routes/github.js
+++ src/routes/github.js
@@ -17,6 +17,7 @@ github.use('/webhook',
 
   (req, res, next) => {
     req.ghEvent = {
+      receivedAt: (new Date()).toJSON(),
       id: req.headers['x-github-delivery'],
       type: req.headers['x-github-event'],
     }
@@ -33,29 +34,36 @@ github.post('/webhook', (req, res, next) => {
 })
 
 github.post('/webhook/pull_request',
-  validate('body', Joi.object({
-    action: Joi.string().valid('opened'),
-    pull_request: Joi.object({
-      url: Joi.string().uri().description('api url'),
-      id: Joi.number().description('api/object id'),
-      number: Joi.number().description('repo-relative number'),
-      state: Joi.string().valid('open', 'closed'),
-      user: Joi.object({
-        login: Joi.string().description('username'),
+  validate('body', Joi
+    .object()
+    .rename('pull_request', 'pullRequest')
+    .keys({
+      action: Joi.string().valid('opened'),
+      pullRequest: Joi.object({
         url: Joi.string().uri().description('api url'),
+        id: Joi.number().description('api/object id'),
+        number: Joi.number().description('repo-relative number'),
+        state: Joi.string().valid('open', 'closed'),
+        user: Joi.object({
+          login: Joi.string().description('username'),
+          url: Joi.string().uri().description('api url'),
+        }),
+        head: Joi.object().description('latest user commit').keys({
+          sha: Joi.string(),
+        }),
+        base: Joi.object().description('target branch').keys({
+          ref: Joi.string().description('branch name'),
+          sha: Joi.string(),
+        }),
       }),
-      head: Joi.object().description('latest user commit').keys({
-        sha: Joi.string(),
-      }),
-      base: Joi.object().description('target branch').keys({
-        ref: Joi.string().description('branch name'),
-        sha: Joi.string(),
-      }),
-    }),
-    repository: Joi.object({
-      full_name: Joi.string().description('org/project formatted name'),
-    }),
-  })),
+      repository: Joi
+        .object()
+        .rename('full_name', 'fullName')
+        .keys({
+          fullName: Joi.string().description('org/project formatted name'),
+        }),
+    })
+  ),
 
   async (req, res) => {
     res.json(await req.app.es.index({
@@ -63,7 +71,7 @@ github.post('/webhook/pull_request',
       type: req.ghEvent.type,
       id: req.ghEvent.id,
       body: {
-        received_at: (new Date()).toJSON(),
+        ...req.ghEvent,
         ...req.body,
       },
     }))
