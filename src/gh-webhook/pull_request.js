a8021ef3497f34b04eadfbccdb81c4545337662c
spalger <email@spalger.com>
2016-07-16 17:36:31 -0700

    revert case-swapping, preserve github_casing

--- src/gh-webhook/pull_request.js
+++ src/gh-webhook/pull_request.js
@@ -5,36 +5,29 @@ import { Router, validate } from '../utils'
 const router = new Router()
 
 router.post('/pull_request',
-  validate('body', Joi
-    .object()
-    .rename('pull_request', 'pullRequest')
-    .keys({
-      action: Joi.string().valid('opened'),
-      pullRequest: Joi.object({
+  validate('body', Joi.object().keys({
+    action: Joi.string().valid('opened'),
+    pull_request: Joi.object({
+      url: Joi.string().uri().description('api url'),
+      id: Joi.number().description('api/object id'),
+      number: Joi.number().description('repo-relative number'),
+      state: Joi.string().valid('open', 'closed'),
+      user: Joi.object({
+        login: Joi.string().description('username'),
         url: Joi.string().uri().description('api url'),
-        id: Joi.number().description('api/object id'),
-        number: Joi.number().description('repo-relative number'),
-        state: Joi.string().valid('open', 'closed'),
-        user: Joi.object({
-          login: Joi.string().description('username'),
-          url: Joi.string().uri().description('api url'),
-        }),
-        head: Joi.object().description('latest user commit').keys({
-          sha: Joi.string(),
-        }),
-        base: Joi.object().description('target branch').keys({
-          ref: Joi.string().description('branch name'),
-          sha: Joi.string(),
-        }),
       }),
-      repository: Joi
-        .object()
-        .rename('full_name', 'fullName')
-        .keys({
-          fullName: Joi.string().description('org/project formatted name'),
-        }),
-    })
-  ),
+      head: Joi.object().description('latest user commit').keys({
+        sha: Joi.string(),
+      }),
+      base: Joi.object().description('target branch').keys({
+        ref: Joi.string().description('branch name'),
+        sha: Joi.string(),
+      }),
+    }),
+    repository: Joi.object().keys({
+      full_name: Joi.string().description('org/project formatted name'),
+    }),
+  })),
 
   async (req, res) => {
     res.json(await req.app.es.index({
